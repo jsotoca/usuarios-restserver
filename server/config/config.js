cc608d62520cf2d74dbdf6972567a002cdeb3625
jsotoca <jsotoca@unprg.edu.pe>
2020-02-13 13:56:50 -0500

    configurando variable de entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -3,4 +3,4 @@ process.env.PORT = process.env.PORT || 3000;
 
 // BASE DE DATOS
 let estado = process.env.NODE_ENV || 'dev';
-(estado==='dev')?process.env.MONGO_URI='mongodb://localhost:27017/buscapatas':process.env.MONGO_URI='mongodb+srv://jsotoca:OnbSGdcsxjgdTxtn@cluster0-jmx1q.mongodb.net/buscapatas';
+(estado==='dev')?process.env.MONGO_URI='mongodb://localhost:27017/buscapatas':process.env.MONGO_URI=process.env.MONGO_URI;
